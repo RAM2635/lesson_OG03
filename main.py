6569e7a0bd8426a39aae9f11ee61e8b37d6882fd
RAM2635 <sigreys@mail.ru>
2024-09-06 13:24:01 +0300

    add screen

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from operator import iconcat
+
 import pygame
+from pygame.display import set_allow_screensaver
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode(SCREEN_WIDTH,SCREEN_HEIGHT)
+pygame.display.set_caption("Игра ТИР")
+icon = pygame.image.load("screns/oppo.jpg")
 running = True
 while running:
     pass
